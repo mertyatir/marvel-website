da6253b123c6396dd9e290207a70f7e0a67dd354
Mert Yatır <68438412+mertyatir@users.noreply.github.com>
2025-08-29 01:37:51 +0300

    Update marvel.js

--- src/marvel.js
+++ src/marvel.js
@@ -1,2 +1,2 @@
-export const private_key = "ENTER_PRIVATE_KEY_HERE"
-export const public_key = "d50071621cd7dd025f681b61ee800d3e"
+export const private_key = process.env.PRIVATE_KEY;
+export const public_key = process.env.PUBLIC_KEY;
